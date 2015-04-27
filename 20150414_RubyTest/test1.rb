e9264fe37835bba112a8a3d143e3cd7cf89bf7a5
Yudai Tsukamoto <yudai.tsukamoto@litalico.co.jp>
2015-04-27 16:27:09 +0900

    "update"

--- 20150414_RubyTest/test1.rb
+++ 20150414_RubyTest/test1.rb
@@ -1,33 +1,38 @@
 def prime_number_proc
-    primes = []
+  primes = []
 
-    f = Proc.new do
-        prime = primes[-1].to_i
-        loop do
-            prime += 1
-            if prime?(prime)
-                primes << prime 
-                break
-            end
-        end
-        primes
-    end	  
-    f
+  f = proc do
+    if primes.any?
+      number = primes.last + 1
+    else
+      number = 2
+    end
+    loop do
+      if prime?(number)
+        primes << number
+        break
+      end
+      number += 1
+    end
+    primes
+  end
+  f
 end
 
-def prime?(prime)
-    return true if prime == 2
-    return false if prime < 2 || prime % 2 == 0
+def prime?(number)
+  return true if number == 2
+  return false if number < 2 || number.even?
 
-    3.step(Math.sqrt(prime), 2) do |divisor|
-        if prime % divisor == 0
-            return false
-        end
+  3.step(Math.sqrt(number), 2) do |divisor|
+    if number % divisor == 0
+      return false
     end
-    true
+  end
+  true
 end
 
 fun = prime_number_proc
-10.times do 
-   p fun.call
+
+10.times do
+  p fun.call
 end
