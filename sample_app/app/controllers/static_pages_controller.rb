df3ae92e3df102738843998b2ae3704f2d33c5a3
Yudai Tsukamoto <yudai.tsukamoto@litalico.co.jp>
2015-06-04 21:03:23 +0900

    update

--- sample_app/app/controllers/static_pages_controller.rb
+++ sample_app/app/controllers/static_pages_controller.rb
@@ -3,7 +3,6 @@ class StaticPagesController < ApplicationController
     if signed_in?
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page:params[:page])
-      binding.pry
     end
   end
 
